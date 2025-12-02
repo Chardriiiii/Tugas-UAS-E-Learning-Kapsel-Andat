e14ca09c25f3878b7fa7d49118944219a4d1fb5d
Adyan <rahahaha.dyan@gmail.com>
2025-12-02 13:51:41 +0700

    commit 4

--- database.py
+++ database.py
@@ -1,23 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-from dotenv import load_dotenv
-import os
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
+# Ganti password sesuai konfigurasi MySQL Anda
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Babikambing1@localhost:3306/student_performance_db"
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
+# Dependency untuk koneksi DB di setiap request
 def get_db():
     db = SessionLocal()
     try:
